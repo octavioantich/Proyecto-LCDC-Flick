edb64939abe2402d322c07c0a0f481968509281c
Juan Dingevan <jidingevan@gmail.com>
2022-04-18 12:58:48 -0300

    Square ahora puede tener como texto "" o el emoji de una estrella.
    Esto es una preparación para cuando se pueda seleccionar un origen con un click. Este origen se marcará con el emoji.

--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { colorToCss } from './Game';
 
+//⭐
+
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          text: ""
+        };
+    }
+
+    addStar = () => {
+        this.setState({text: "⭐"});
+     };
+
     render() {
         return (
-            <div style={{ backgroundColor: colorToCss(this.props.value) }} />
+            <div style={{ backgroundColor: colorToCss(this.props.value) }}>
+                {this.state.text}
+            </div>
         );
     }
 }
